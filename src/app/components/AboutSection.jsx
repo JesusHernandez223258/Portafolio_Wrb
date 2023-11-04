a6d7dae6f233a72281ebf7fefccb826a39ed95f2
Jesus Hernandez <223258@ids.upchiapas.edu.mx>
2023-11-03 18:52:24 -0600

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MySQL</li>
+        <li>Sequelize</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Tecnología para el bien común</li>
+        <li>Universidad Politécnica de Chiapas</li>
+      </ul>
+    ),
+  },
+  /*{
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },*/
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} alt="" />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">Sobre mi</h2>
+          <p className="text-base lg:text-lg">
+            Soy un desarrollador web full stack con pasión por crear
+            Aplicaciones web interactivas y responsivas. tengo experiencia
+            trabajando con JavaScript, React, , Node.js, Express, MySQL,
+            Sequelize, MongoDB, Monsoose, HTML, CSS y Git. Aprendo rápido y
+            siempre estoy Buscando ampliar mis conocimientos y habilidades. Soy
+            un jugador de equipo y Estoy emocionado de trabajar con otros para
+            crear aplicaciones increíbles.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Habilidades{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Educacion{" "}
+            </TabButton>
+
+            {/*<TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>*/}
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
